fd05c83174a4c354d7214887f01ab037cae96e5a
MarineAGLAVE <38038917+MarineAGLAVE@users.noreply.github.com>
2019-02-22 14:35:12 +0100

    Scripts FAIR session 2

---
+++ Dockerfile
@@ -0,0 +1,72 @@
+FROM rocker/binder
+
+## USER
+USER root
+
+## Update
+RUN apt-get update
+
+# Change workdirectory
+ENV HOME /home
+WORKDIR ${HOME}
+
+## Install Conda
+RUN apt-get install -y wget bzip2
+RUN wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+RUN bash Miniconda3-latest-Linux-x86_64.sh -b
+RUN rm Miniconda3-latest-Linux-x86_64.sh
+
+ENV PATH /home/miniconda3/bin:$PATH
+
+## Update
+RUN conda update conda
+RUN conda update --all
+
+## Add chanel
+RUN conda config --add channels conda-forge
+RUN conda config --add channels bioconda
+
+## Install snakemake
+RUN conda install -c bioconda -c conda-forge snakemake
+
+## Install fastqc, bowtie2, htseq and samtools
+RUN conda install -y fastqc bowtie2 htseq samtools
+
+## Add R chanel
+conda config --add channels r
+#conda install r-readxl #j'ai fait cette commande dans la foulée mais je ne sais pas si elle est nécessaire.
+
+## Install Bioconductor package
+RUN Rscript -e 'if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager") ; BiocManager::install("DESeq2", version = "3.8")'
+RUN Rscript -e 'if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager") ; BiocManager::install("edgeR", version = "3.8")'
+RUN Rscript -e 'if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager") ; BiocManager::install("genefilter", version = "3.8")'
+
+# Install CRAN package
+RUN Rscript -e "install.packages('devtools', repos='https://cran.rstudio.com/', dependencies = TRUE)" \
+    && rm -rf /tmp/downloaded_packages/ /tmp/*.rds
+RUN Rscript -e "install.packages(c('shinydashboard','DT', 'FactoMineR', 'corrplot','plotly'), repos='https://cran.rstudio.com/', dependencies = TRUE)" \
+    && rm -rf /tmp/downloaded_packages/ /tmp/*.rds
+RUN Rscript -e "install.packages(c('shinyWidgets','colourpicker'), repos='https://cran.rstudio.com/', dependencies = TRUE)" \
+    && rm -rf /tmp/downloaded_packages/ /tmp/*.rds
+RUN Rscript -e "install.packages(c('shinycssloaders'), repos='https://cran.rstudio.com/', dependencies = TRUE)" \
+    && rm -rf /tmp/downloaded_packages/ /tmp/*.rds
+
+# Install Sarstools
+RUN Rscript -e 'library(devtools) ; install_github("PF2-pasteur-fr/SARTools", build_vignettes=TRUE)'
+
+# Install aspera
+RUN conda install -c hcc aspera-cli
+
+# Install fastq dump
+
+RUN wget --quiet "https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.9.2/sratoolkit.2.9.2-ubuntu64.tar.gz"
+RUN tar -xzf sratoolkit.2.9.2-ubuntu64.tar.gz
+RUN rm sratoolkit.2.9.2-ubuntu64.tar.gz
+ENV PATH /home/sratoolkit.2.9.2-ubuntu64/bin:$PATH
+
+## USER
+USER rstudio
+ENV HOME /home/rstudio
+WORKDIR ${HOME}
+
+CMD jupyter notebook --ip 0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
---
+++ FAIR_script.sh
@@ -0,0 +1,92 @@
+##------------------------------------------------------------------------------
+## FAIR script
+## Author: T. Denecker & C. Toffano-Nioche
+## Affiliation: I2BC
+## Aim: A workflow to process RNA-Seq.
+## Organism : O. tauri
+## Date: Jan 2019
+## Step :
+## 1- Create tree structure
+## 2- Download data from SRA
+## 3- Run workflow (snakemake)
+##------------------------------------------------------------------------------
+
+
+echo "=============================================================="
+echo "Creation of tree structure"
+echo "=============================================================="
+
+mkdir Project
+mkdir Project/samples
+mkdir Project/annotations
+mkdir Project/bowtie2
+mkdir Project/fastqc
+mkdir Project/genome
+mkdir Project/graphics
+mkdir Project/htseq
+mkdir Project/reference
+mkdir Project/samtools
+
+echo "=============================================================="
+echo "Download data from SRA"
+echo "=============================================================="
+
+# Get accession number (comment / uncomment to change methods)
+ Accession="$(cut -f7 conditions.txt | tail -n +2)" # ascp method
+# Accession="$(cut -f1 conditions.txt | tail -n +2)" # fastq dump method
+# Accession="$(cut -f6 conditions.txt | tail -n +2)" # wget method
+
+cd Project/samples
+
+IFS=$'\n'       # make newlines the only separator
+for j in $(tail -n +2 ../../conditions.txt) #pour toutes les lignes à partir de la deuxième
+do
+
+    access=$( echo "$j" |cut -f7 )
+    id=$( echo "$j" |cut -f1 )
+
+    echo "--------------------------------------------------------------"
+    echo ${id}
+    echo "--------------------------------------------------------------"
+
+    ascp -QT --file-checksum=md5 --file-manifest=text --file-manifest-path=. -l 300m -P33001 -i /home/marine.aglave/anaconda3/envs/FAIR/etc/asperaweb_id_dsa.openssh ${access} .
+
+    md5_local="$(md5sum $id.fastq.gz | cut -d' ' -f1)"
+    echo $md5_local
+
+    if grep -q $md5_local *.txt
+    then
+        echo "Done"
+    else
+        rm $id.fastq.gz
+        access=$( echo "$j" |cut -f6 )
+        wget ${access} # wget method
+    fi
+    rm *.txt
+done
+
+cd ../..
+
+echo "=============================================================="
+echo "Download annotations"
+echo "=============================================================="
+
+wget https://raw.githubusercontent.com/thomasdenecker/FAIR_Bioinfo/master/Data/O.tauri_annotation.gff -P Project/annotations
+
+echo "=============================================================="
+echo "Download genome"
+echo "=============================================================="
+
+wget https://raw.githubusercontent.com/thomasdenecker/FAIR_Bioinfo/master/Data/O.tauri_genome.fna -P Project/genome
+get
+echo "=============================================================="
+echo "Snakemake"
+echo "=============================================================="
+
+snakemake
+
+echo "=============================================================="
+echo "Shiny app"
+echo "=============================================================="
+
+R -e "shiny::runApp('R-code', port=4444)"
